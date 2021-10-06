209d42344d61b31e74a906c78c2bb75fa5df0676
watidarmawan <82010926+watidarmawan@users.noreply.github.com>
2021-10-06 19:29:10 +0700

    minggu4

---
+++ GaugeWatiDarmawan/app/src/main/java/com/example/gaugewatidarmawan/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gaugewatidarmawan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
